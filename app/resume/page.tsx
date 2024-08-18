539693e6540485f9ad77e5eea574d940c4f76802
alexsalce <101913154+alexsalce@users.noreply.github.com>
2024-08-18 20:29:04 +0000

    fixes to resume page.tsx prettier

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -7,16 +7,15 @@ import { Resume, allResumes } from 'contentlayer/generated'
 import ResumeLayout from '@/layouts/ResumeLayout'
 
 export default function Page() {
-    const resume = allResumes.find((p) => p.slug === 'default') as Resume
-    // Convert resume.toc from string to TOC[]
-    return (
-        <>
-        <SectionContainer>
-            <ResumeLayout toc={resume.toc}>
-            <MDXLayoutRenderer code={resume.body.code} components={components} toc={resume.toc} />
-            </ResumeLayout>
-
-        </SectionContainer>
-        </>
-    )
-    }
+  const resume = allResumes.find((p) => p.slug === 'default') as Resume
+  // Convert resume.toc from string to TOC[]
+  return (
+    <>
+      <SectionContainer>
+        <ResumeLayout toc={resume.toc}>
+          <MDXLayoutRenderer code={resume.body.code} components={components} toc={resume.toc} />
+        </ResumeLayout>
+      </SectionContainer>
+    </>
+  )
+}
